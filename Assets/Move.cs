f2f57aa62975a02f45e685620647e727fb383cbf
NatalyEliyahu <nataly.eliyahu@gmail.com>
2015-02-17 15:05:59 +0200

    Adding initial Unity project - Assets and ProjectSettings

---
+++ Assets/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public Vector3 direction = Vector3.forward;
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		speed = 50.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += direction.normalized * speed * Time.deltaTime;
+	}
+}
---
+++ Assets/MoveLeftAndRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		// Set speed to 5
+		speed = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Move the object according to player input and speed (per second)
+		transform.position += Vector3.right * speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+	}
+}
