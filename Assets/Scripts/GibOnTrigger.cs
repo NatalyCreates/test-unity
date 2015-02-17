8e581bfac81bca295dbec91314117aa32aa6c8d1
NatalyEliyahu <nataly.eliyahu@gmail.com>
2015-02-17 20:03:38 +0200

    Pillars randomly spawning, player dies and explodes when collides with pillar, pillar objects destroyed when we pass them.

--- Assets/Scripts/GibOnTrigger.cs
+++ Assets/Scripts/GibOnTrigger.cs
@@ -6,7 +6,7 @@ public class GibOnTrigger : MonoBehaviour {
 	public GameObject gib;
 
 	void OnTriggerEnter() {
-		// Debug.Log ("Shot the enemy!");
+		Debug.Log ("Shot the enemy!");
 		Instantiate(gib, transform.position, Quaternion.identity);
 		Destroy(gameObject);
 	}
---
+++ Assets/Scripts/RandomlySpawn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomlySpawn : MonoBehaviour {
+
+	public GameObject thing;
+
+	public float minHorizPos = -10.0f;
+	public float maxHorizPos = 10.0f;
+	public float minVertiPos = -10.0f;
+	public float maxVertiPos = 10.0f;
+
+	public float minSpawnTime = 1.0f;
+	public float maxSpawnTime = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("SpawnNow", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+
+	void SpawnNow() {
+		Instantiate(thing,
+		            transform.position + new Vector3(Random.Range(minHorizPos,maxHorizPos),
+		                                 Random.Range(minVertiPos,maxVertiPos)),
+		            Quaternion.identity);
+		Invoke("SpawnNow", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
