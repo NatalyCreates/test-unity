97a16361d23ef9974a7172ff705ed7174dbe04c0
NatalyEliyahu <nataly.eliyahu@gmail.com>
2015-02-21 15:04:24 +0200

    Added high score that saves locally.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,34 +16,53 @@ public class GameManager : MonoBehaviour {
 
 	private static float highScore = 0.0f;
 
+	private bool hasSaved = false;
+
 	// Use this for initialization
 	void Start() {
 		Instance = this;
+		LoadHighScore();
 	}
 	
 	// Update is called once per frame
 	void Update() {
 
-		score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
-		if (score > highScore) {
-			highScore = score;
-		}
-
 		if(GameObject.FindGameObjectWithTag("Player") == null) {
 			gameOver = true;
 		}
 		if (gameOver) {
+			if (!hasSaved) {
+				SaveHighScore();
+				hasSaved = true;
+			}
 			if(Input.anyKeyDown) {
 				Application.LoadLevel(Application.loadedLevel);
 			}
 		}
+		else { // game isn't over
+			score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+			if (score > highScore) {
+				highScore = score;
+			}
+		}
+	}
+
+	void SaveHighScore() {
+		PlayerPrefs.SetInt("HighScore", (int)highScore);
+		PlayerPrefs.Save();
+	}
+
+	void LoadHighScore() {
+		highScore = PlayerPrefs.GetInt("HighScore");
 	}
 
 	void OnGUI() {
 		GUIStyle style = new GUIStyle();
 		style.fontSize = 30;
 		int currentScore = (int)score;
+		int currentHighScore = (int)highScore;
 		GUILayout.Label("Score: " + currentScore.ToString(), style);
+		GUILayout.Label("High-Score: " + currentHighScore.ToString(), style);
 		//GUILayout.Label(score.ToString());
 		if (gameOver == true) {
 			GUILayout.Label("Game Over! Press any key to reset!", style);
